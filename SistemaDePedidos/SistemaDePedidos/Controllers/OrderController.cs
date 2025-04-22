12200dd13e32a82695b397e2e143504662b2a755
Brian Courel <briiancourel@gmail.com>
2025-04-22 19:48:02 -0300

    hello word

---
+++ SistemaDePedidos/SistemaDePedidos/Controllers/OrderController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Controllers;
+using Models;
+using Views;
+
+namespace SistemaDePedidos.Controllers
+{
+    class OrderController
+    {
+        private ClientController cController;
+        private ProductController pController;
+        public Order currentOrder {  get; set; }
+
+        public OrderController()
+        {
+            this.cController = new ClientController();
+            this.pController = new ProductController();
+        }
+
+        public void CreateOrder()
+        {
+            currentOrder = new Order();
+            currentOrder.client = ClientView.LoadClient();
+            currentOrder.setProductList(OrderView.LoadProductList());
+        }
+    }
+}
